7edda6b9448d060692ad407a53a8717a49dceefd
Defu5er <56508188+Defu5er@users.noreply.github.com>
2022-01-31 20:02:45 +0100

    Main Class added
    Errors 100%

---
+++ SudokuHighscore/src/NoahArmanProject/HighscoreAdmin.java
@@ -0,0 +1,16 @@
+package NoahArmanProject;
+
+public class HighscoreAdmin{
+    static void submit(String name, String dateTime, String level, Integer finishTime){
+    }
+    static void save(){
+    }
+    static void load(){
+    }
+    static void Highscore[] getHighscore(String level){
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
